db64f4b35f8850d2bf7b88258b9fb0ff350a8a7f
Henri Rosten <henri.rosten@unikie.com>
2024-01-04 10:21:27 +0200

    Use nixmeta from tiiuae/sbomnix
    - Use nixmeta from tiiuae/sbomnix
    - Pin the nixmeta version
    - Fix shellcheck warnings
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- nixmeta.sh
+++ nixmeta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+# SPDX-FileCopyrightText: 2023-2024 Technology Innovation Institute (TII)
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -17,17 +17,22 @@ set -o pipefail # exit if any pipeline command fails
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Output file path
 OUT_FILE="$SCRIPT_DIR/data/nixmeta.csv"
+# Pin to the following sbomnix revision
+REV="bbfe0949ab8875f5c98dcdd1d81d2721676b4b26"
 
 # nixos-unstable
-nix run --refresh github:henrirosten/sbomnix?ref=nixmeta#nixmeta -- \
-  --flakeref=github:NixOS/nixpkgs?ref=nixos-unstable --out=$OUT_FILE --append --verbose=2
+nix run --refresh "github:tiiuae/sbomnix?rev=$REV#nixmeta" -- \
+  --flakeref="github:NixOS/nixpkgs?ref=nixos-unstable" --out="$OUT_FILE" \
+  --append --verbose=2
 
 # release-23.11
-nix run --refresh github:henrirosten/sbomnix?ref=nixmeta#nixmeta -- \
-  --flakeref=github:NixOS/nixpkgs?ref=release-23.11 --out=$OUT_FILE --append --verbose=2
+nix run --refresh "github:tiiuae/sbomnix?rev=$REV#nixmeta" -- \
+  --flakeref="github:NixOS/nixpkgs?ref=release-23.11" --out="$OUT_FILE" \
+  --append --verbose=2
 
 # release-23.05
-nix run --refresh github:henrirosten/sbomnix?ref=nixmeta#nixmeta -- \
-  --flakeref=github:NixOS/nixpkgs?ref=release-23.05 --out=$OUT_FILE --append --verbose=2
+nix run --refresh "github:tiiuae/sbomnix?rev=$REV#nixmeta" -- \
+  --flakeref="github:NixOS/nixpkgs?ref=release-23.05" --out="$OUT_FILE" \
+  --append --verbose=2
 
 ################################################################################
